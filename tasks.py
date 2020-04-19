c41c428cb7db9bee10971c1bbea8e08267ddfe7e
zkan <zkan.cs@gmail.com>
2020-04-19 22:52:50 +0700

    Update dependencies and define shared task :bear:

--- tasks.py
+++ tasks.py
@@ -1,12 +1,18 @@
 import time
 
 from celery import Celery
+from celery import shared_task
 
 
-app = Celery('tasks', broker='redis://localhost:6379')
+app = Celery('gatuk', broker='redis://localhost:6379')
 
 
-@app.task
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10.0, hello.s('Kan'), name='add every 10')
+
+
+@shared_task
 def hello(name):
     time.sleep(5)
     return f'Hello, {name}'
